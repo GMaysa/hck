0ccf7400080c59ed33cc1b6b647c7f4b8978731e
Maysa Putra <GMaysa>
2022-09-01 09:53:18 +0800

    #3

--- sesi-3/homework.js
+++ sesi-3/homework.js
@@ -1,22 +1,26 @@
 const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true]
 
-let makeAllCaps = (arr) => {
-    return new Promise((resolve)=>{
-        resolve(String.prototype.toUpperCase.apply(arr))
-    })
+const sortWords = (arr) => {
+    return arr.sort()
 }
 
-let sortWords = (arr) => {
-
+let makeAllCaps = (arr) => {
+    return new Promise((resolve)=>{
+        const result = arr.map((upCase) => {
+            if (typeof upCase === "string")return upCase.toUpperCase()
+            else return upCase
+        })
+        resolve(result)
+    })
 }
 
 makeAllCaps(arrayOfWords)
 .then(sortWords)
-.then((result) => consoe.log(result))
-.catch(error => consoe.log(error))
+.then((result) => console.log(result))
+.catch(error => console.log(error))
 
 makeAllCaps(complicatedArray)
 .then(sortWords)
-.then((result) => consoe.log(result))
-.catch(error => consoe.loq(error))
+.then((result) => console.log(result))
+.catch(error => console.log(error))
